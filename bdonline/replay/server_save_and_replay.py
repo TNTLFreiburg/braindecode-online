9ec47aa154c456a07a676ca839f7b5db24af7e80
HenrikBons <henrikbons@gmail.com>
2019-10-16 13:55:49 +0200

    Update server_save_and_replay.py

--- bdonline/replay/server_save_and_replay.py
+++ bdonline/replay/server_save_and_replay.py
@@ -106,25 +106,6 @@ LOG_FILENAME = 'stored_experiment.txt'
 logging.basicConfig(filename=LOG_FILENAME, filemode='w', level=logging.INFO)
 log = logging.getLogger(__name__)
 
-TCP_SENDER_EEG_NCHANS = 65  # number of channels to send to braindecode-online, includes labels
-TCP_SENDER_EEG_NSAMPLES = 10
-CHAN_NAMES = ['Fp1', 'Fpz', 'Fp2', 'AF7',     # channel names send to braindecode-online
-                  'AF3', 'AF4', 'AF8', 'F7',
-                  'F5', 'F3', 'F1', 'Fz', 'F2', 'F4', 'F6', 'F8', 'FT7', 'FC5',
-                  'FC3',
-                  'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'FT8', 'M1', 'T7', 'C5',
-                  'C3',
-                  'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'M2', 'TP7', 'CP5', 'CP3',
-                  'CP1', 'CPz', 'CP2', 'CP4', 'CP6', 'TP8', 'P7', 'P5', 'P3',
-                  'P1',
-                  'Pz', 'P2', 'P4', 'P6', 'P8', 'PO7', 'PO5', 'PO3', 'POz',
-                  'PO4',
-                  'PO6', 'PO8', 'O1', 'Oz', 'O2', 'marker']
-
-
-path = 'D:\\DLVR\\Data\\subjM\\' #the data we want to replay
-files = ['block4.xdf']
-DATA_AND_LABELS  = xdf_to_bd.dlvr_braindecode(path,files, -1, 250) #Load in all data and labels
 
 class PredictionSender(object):
     def __init__(self, socket):
